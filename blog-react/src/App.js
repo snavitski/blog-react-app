fcb4e496deea58b584635f70127956c74b5b0429
Siarhei Navitski <sergiej.nowicki@gmail.com>
2022-05-25 09:48:47 +0200

    final 2

--- blog-react/src/App.js
+++ blog-react/src/App.js
@@ -10,23 +10,22 @@ import Footer from "./components/views/Footer";
 import NotFound from "./components/pages/NotFound";
 import { Container } from "react-bootstrap";
 
-
 const App = () => {
 	return (
 		<main>
 			<NavBar />
-				<Container>
-					<Routes>
-						<Route path='/' element={<Home />} />
-						<Route path='/podst/:id' element={<PostPage />} />
-						<Route path='/post/add' element={<PostAdd />} />
-						<Route path='/post/edit/:id' element={<PostEdit />} />
-						<Route path='/about' element={<About />} />
-						<Route path='*' element={<NotFound />} />
-					</Routes>
-					<Footer />
-				</Container>
-				</main>
+			<Container>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/post/:id' element={<PostPage />} />
+					<Route path='/post/add' element={<PostAdd />} />
+					<Route path='/post/edit/:id' element={<PostEdit />} />
+					<Route path='/about' element={<About />} />
+					<Route path='*' element={<NotFound />} />
+				</Routes>
+				<Footer />
+			</Container>
+		</main>
 	);
 };
 
